10af733600dafd7aea9eab47ffea222b0c0de54e
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-06-22 17:32:43 +0600

    add theming

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: [
-    require("postcss-easy-import")({ prefix: "_" }) // keep this first
+    require("postcss-easy-import")({ prefix: "_" }), // keep this first
+    require("postcss-cssnext")({
+      features: {
+        customProperties: {
+          variables: {
+            colorPrimary: "#51a9f2"
+          }
+        }
+      }
+    })
   ]
 };
